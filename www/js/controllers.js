fbe4b238eb85d94fc7586fb4d0384a2f79341e0f
luanmenezes <luan_menezes90@hotmail.com>
2017-01-27 16:42:34 -0200

    adicionando cordovacamera

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -89,11 +89,24 @@ angular.module('starter')
         });
         
 angular.module('starter')
-        .controller('PerfilController', function ($rootScope, $scope){
+        .controller('PerfilController', function ($rootScope, $scope, $cordovaCamera){
             $scope.estaEditando = false;
             $scope.textoBotao = 'Editar';
             
             $scope.usuarioLogado = $rootScope.usuario;
+            
+            $scope.tirarFoto = function (){
+                var opcoes = {
+                    quality : 70,
+                    correctOrientation: true
+                }
+                
+                $cordovaCamera.getPicture(opcoes).then(function (foto){
+                    $scope.caminhoFoto = foto;
+                }, function (erro){
+                    
+                });
+            }
             
             $scope.acaoBotao = function (){
                 if($scope.estaEditando){
