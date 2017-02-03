6ebdc56f5829253ecb767bff3c318d590e71ba2a
luanmenezes <luan_menezes90@hotmail.com>
2017-02-03 08:48:09 -0200

    merge

--- www/js/controllers/finalizarpedido.controller.js
+++ www/js/controllers/finalizarpedido.controller.js
@@ -1,8 +1,21 @@
 angular.module('starter')
-    .controller('FinalizarPedidoController', function ($stateParams, $scope, $ionicPopup, $state, CarroService, $ionicHistory) {
+    .controller('FinalizarPedidoController', function ($stateParams, $scope, $ionicPopup, $state, CarroService, $ionicHistory, ionicDatePicker) {
         $scope.carroFinalizado = angular.fromJson($stateParams.carro);
 
         $scope.pedido = {};
+
+        $scope.dtAgendamento;
+
+        $scope.abrirPopupCalendario = function (){
+            var config = {
+                callback: function (data){
+                    $scope.dtAgendamento = new Date(data);
+                },
+                mondayFirst: false,
+                weeksList : ['D','S', 'T', 'Q', 'Q', 'S', 'S']
+            };
+            ionicDatePicker.openDatePicker(config);
+        };
 
         $scope.finalizarPedido = function () {
 
@@ -12,7 +25,8 @@ angular.module('starter')
                     preco: $scope.carroFinalizado.preco,
                     nome: $scope.pedido.nome,
                     endereco: $scope.pedido.endereco,
-                    email: $scope.pedido.email
+                    email: $scope.pedido.email,
+                    dtagenda: $scope.dtAgendamento
                 }
             };
 
